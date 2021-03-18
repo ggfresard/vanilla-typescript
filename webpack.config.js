b8fdbc514640ed0f0c608cf1da28f0c182beedf1
Gabriel <ggfresard@gmail.com>
2021-03-18 20:58:35 -0300

    initial

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path")
+
+module.exports = {
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    publicPath: "/",
+    contentBase: path.join(__dirname, "dist"),
+    hot: true,
+    serveIndex: true,
+    index: "index.html",
+  },
+}
